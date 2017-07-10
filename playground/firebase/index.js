bda9e398a16f1c43939ceb1dd9dbb71523c2e5b3
adsznzhang <adszn@hotmail.com>
2017-07-10 21:46:21 +0800

    play with firebase

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 var config = {
     apiKey: "AIzaSyCKif6PLvUNN967ppcWzcv5l7-smP5TySs",
@@ -10,6 +10,48 @@ var config = {
 };
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-    appName: 'TODO App'
+var firebaseRef = firebase.database().ref();
+
+firebaseRef.set({
+    app: {
+        name: 'Todo APP',
+        version: '1.0.0'
+    },
+    isRunning: true,
+    user: {
+        name: 'Andrew',
+        age: 25
+    }
+});
+
+
+// firebaseRef.child('app').once('value').then((snapshot) => {
+//     console.log('GOT entire database',snapshot.val());
+// },(e) => {
+//     console.log('Unable to fetch value', e);
+// });
+
+
+
+// var logData = (snapshot) => {
+//     console.log('GOT value', snapshot.val())
+// }
+
+// firebaseRef.on('value', logData)
+
+// firebaseRef.off('value',logData)
+
+// firebaseRef.update({isRunning: false})
+
+var logData = (snapshot) => {
+    console.log('Got value', snapshot.val());
+};
+firebaseRef.child('user').on('value', logData);
+
+firebaseRef.child('user').update({
+    name: 'ANDRAIN'
+})
+
+firebaseRef.child('app').update({
+    name: 'TODO APP'
 })
